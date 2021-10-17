40019516f05b4b2b0b9d0e72066b2b906db89ea1
itytophile <imran.tierce@protonmail.com>
2021-10-17 15:08:28 +0200

    Slave can connect to server

--- src/slave.rs
+++ src/slave.rs
@@ -1,1 +1,26 @@
-fn main() {}
+use std::env;
+
+use futures::SinkExt;
+use futures_util::StreamExt;
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+
+#[tokio::main]
+async fn main() {
+    let connect_addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| panic!("this program requires at least one argument"));
+
+    let url = url::Url::parse(&connect_addr).unwrap();
+
+    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+    println!("WebSocket handshake has been successfully completed");
+
+    let (mut write, mut read) = ws_stream.split();
+
+    write
+        .send(Message::Text("slave".to_owned()))
+        .await
+        .expect("issou");
+    let lol = read.next().await.unwrap().expect("saucisse");
+    dbg!(lol);
+}
